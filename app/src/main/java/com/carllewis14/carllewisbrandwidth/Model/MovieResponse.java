87a44392bbc869a8c9847191546eb28f17672b15
carl Lewis <carl.lewis@incisivemedia.com>
2017-04-27 10:43:21 +0100

    Added the POJO classes Movie and MovieResponse

---
+++ app/src/main/java/com/carllewis14/carllewisbrandwidth/Model/MovieResponse.java
@@ -0,0 +1,53 @@
+package com.carllewis14.carllewisbrandwidth.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * This Class handles the extra fields from API feed that are not in the Movie POJO
+ */
+
+public class MovieResponse {
+
+    @SerializedName("page")
+    private int page;
+    @SerializedName("results")
+    private List<Movie> results;
+    @SerializedName("total_results")
+    private int totalResults;
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
